87393c489f1950ef96c57972e05f377dc3f3882c
INABA Hitoshi <ina@cpan.org>
2013-01-27 16:26:22 +0000

    Import of INA/Char-INFORMIXV6ALS-0.86 from CPAN.
    gitpan-cpan-distribution: Char-INFORMIXV6ALS
    gitpan-cpan-version:      0.86
    gitpan-cpan-path:         INA/Char/INFORMIXV6ALS/Char-INFORMIXV6ALS-0.86.tar.gz
    gitpan-cpan-author:       INA
    gitpan-cpan-maturity:     released

--- Einformixv6als.pm
+++ Einformixv6als.pm
@@ -27,7 +27,7 @@ BEGIN {
 # (and so on)
 
 BEGIN { eval q{ use vars qw($VERSION) } }
-$VERSION = sprintf '%d.%02d', q$Revision: 0.85 $ =~ /(\d+)/xmsg;
+$VERSION = sprintf '%d.%02d', q$Revision: 0.86 $ =~ /(\d+)/xmsg;
 
 BEGIN {
     my $PERL5LIB = __FILE__;
@@ -5093,7 +5093,8 @@ sub INFORMIXV6ALS::reverse(@) {
 #
 sub INFORMIXV6ALS::getc(;*@) {
 
-    my $fh = @_ ? qualify_to_ref(shift) : \*STDIN;
+    my($package) = caller;
+    my $fh = @_ ? qualify_to_ref(shift,$package) : \*STDIN;
     croak 'Too many arguments for INFORMIXV6ALS::getc' if @_ and not wantarray;
 
     my @length = sort { $a <=> $b } keys %range_tr;
